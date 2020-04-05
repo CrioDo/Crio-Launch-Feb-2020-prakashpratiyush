d697968b77d754ade483e602f0c5063129f0ea36
CrioDo <rahul@criodo.com>
2020-04-05 03:40:33 +0000

    Updates

--- precache-manifest.fbf821c65ab0cf74945aab59dc54c804.js
+++ precache-manifest.fbf821c65ab0cf74945aab59dc54c804.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0b01d178714de29998a206d762e9b3cd",
+    "revision": "dc3d1d340dffa3a794435b618ffb6f5e",
     "url": "/Crio-Launch-Feb-2020-prakashpratiyush/index.html"
   },
   {
-    "revision": "799f3d0e2ba4e6e9158f",
+    "revision": "2da1aad951ea64aa2c8e",
     "url": "/Crio-Launch-Feb-2020-prakashpratiyush/static/css/2.57ddf7ad.chunk.css"
   },
   {
-    "revision": "c73b8b754eda48e10f16",
+    "revision": "608fd90e28c1209831e0",
     "url": "/Crio-Launch-Feb-2020-prakashpratiyush/static/css/main.5db1380d.chunk.css"
   },
   {
-    "revision": "799f3d0e2ba4e6e9158f",
-    "url": "/Crio-Launch-Feb-2020-prakashpratiyush/static/js/2.184ff073.chunk.js"
+    "revision": "2da1aad951ea64aa2c8e",
+    "url": "/Crio-Launch-Feb-2020-prakashpratiyush/static/js/2.6cf499dd.chunk.js"
   },
   {
     "revision": "d466ab9648f013dea081956fc7c904f3",
-    "url": "/Crio-Launch-Feb-2020-prakashpratiyush/static/js/2.184ff073.chunk.js.LICENSE.txt"
+    "url": "/Crio-Launch-Feb-2020-prakashpratiyush/static/js/2.6cf499dd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c73b8b754eda48e10f16",
-    "url": "/Crio-Launch-Feb-2020-prakashpratiyush/static/js/main.c462c6bd.chunk.js"
+    "revision": "608fd90e28c1209831e0",
+    "url": "/Crio-Launch-Feb-2020-prakashpratiyush/static/js/main.734f035b.chunk.js"
   },
   {
     "revision": "ec8ae71e23ee66e70d6d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Crio-Launch-Feb-2020-prakashpratiyush/precache-manifest.140487526884ad9882116061f37b551d.js"
+  "/Crio-Launch-Feb-2020-prakashpratiyush/precache-manifest.fbf821c65ab0cf74945aab59dc54c804.js"
 );
 
 self.addEventListener('message', (event) => {
